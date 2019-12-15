071c0e4e30b2262bee5a737f4fb8aa304a363073
Mike Wittenauer <mikewittenauer@me.com>
2019-12-15 10:50:10 -0500

    Add answers to the front end

--- frontend/src/Question/Question.js
+++ frontend/src/Question/Question.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SubmitAnswer from './SubmitAnswer';
+import auth0Client from '../Auth';
 
 const Question = (props) => {
     const [question, setQuestion] = useState(null);
 
     useEffect(() => {
-        async function fetchQuestion() {
-            const { match: { params } } = props;
-            const question = (await axios.get(`http://localhost:8081/${params.questionId}`)).data;
-
-            setQuestion(question);
-        }
-
-        fetchQuestion();
+        refreshQuestion();
     }, []);
+
+    const refreshQuestion = async () => {
+        const { match: { params } } = props;
+        const question = (await axios.get(`http://localhost:8081/${params.questionId}`)).data;
+        setQuestion(
+            question,
+        );
+    }
+
+    const submitAnswer = async (answer) => {
+        await axios.post(`http://localhost:8081/answer/${question.id}`, {
+            answer,
+        }, {
+            headers: { 'Authorization': `Bearer ${auth0Client.getIdToken()}` }
+        });
+
+        await refreshQuestion();
+    }
 
     if (question === null) return <p>Loading ...</p>;
 
@@ -24,6 +37,7 @@ const Question = (props) => {
                     <h1 className="display-3">{question.title}</h1>
                     <p className="lead">{question.description}</p>
                     <hr className="my-4" />
+                    <SubmitAnswer questionId={question.id} submitAnswer={submitAnswer} />
                     <p>Answers:</p>
                     {
                     question.answers.map((answer, idx) => (
